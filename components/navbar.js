8ff2fa97f978f6fc0d84d59531da619a0213f294
Albert Cahyawan <AlbertCahyawan@gmail.com>
2021-05-06 02:09:31 +0700

    first commit

---
+++ components/navbar.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return ( 
+    <header>
+        <nav className="nav"> 
+          <ul className="nav-link-container" >
+            <li className="nav-link"><Link href="/"><a>Pokemon list</a></Link></li>
+            <li className="nav-link"><Link href="/pokemon-detail"><a>Pokemon Detail</a></Link></li> 
+            <li className="nav-link"><Link href="/my-pokemon-List"><a>My Pokemon List</a></Link></li> 
+          </ul>
+        </nav>  
+    </header> 
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../styles/layout.scss'
+import '../styles/pages.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
