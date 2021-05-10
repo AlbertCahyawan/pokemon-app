267cf0f5aeb3f2f018a4042142a76ab5324efc11
Albert Cahyawan <AlbertCahyawan@gmail.com>
2021-05-10 19:26:19 +0700

    added meta tags

--- components/layout.js
+++ components/layout.js
@@ -5,6 +5,20 @@ export default function Layout({children}) {
   return (
     <div className='Layout'>
       <Head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>     
+        <meta name="description" content="Pokemon app"/>
+
+        <meta property="og:description" content="Pokemon app "/>
+        <meta property="og:image" content="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"/>
+        <meta property="og:url" content="https://albert-cahyawan-pokemon-app.netlify.app/"/>
+        <meta property="og:title" content="albert-cahyawan-pokemon-app"/>  
+
+        <meta name="twitter:title" content="Albert Cahyawan website portofolio"/>
+        <meta name="twitter:description"  content="Pokemon app"/>
+        <meta name="twitter:image" content=" https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"/>
+        <meta property="og:url" content="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"/>
+        <meta name="twitter:card" content="summary_large_image"/>
         <meta name="description" content="Pokemon App" />
         <link rel="icon" href="pokemonIcon.ico" />
 
